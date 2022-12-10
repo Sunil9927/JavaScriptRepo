597ff62cecfc62a5e311f8974a75aede320977a7
Sunil9927 <sunilghotkule18@gmail.com>
2022-12-10 18:25:42 +0530

    Javascrpt files
    swap variables
    functions

---
+++ 01_swap_variables.js
@@ -0,0 +1,32 @@
+var myNumber =100;
+var yourNumber = 200;
+console.log("Before Swap");
+console.log(myNumber,yourNumber);
+var temp = myNumber;
+myNumber = yourNumber;
+yourNumber = temp;
+console.log("After Swap");
+console.log(myNumber,yourNumber);
+
+var sweety= "Sweety";
+var cutie= "Cutie";
+console.log("=====Before Swap=====");
+console.log(sweety,cutie);
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("=====After Swap======");
+console.log(sweety,cutie);
+
+var num1=100;
+var num2=200;
+var num3=300;
+
+console.log("======Before Swap=====");
+console.log(num1,num2,num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("======After Swap======");
+console.log(num1,num2,num3);
